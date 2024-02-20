69bb1a8c0fe24aebb534ff107d88a3f33498efe5
felipecrivera <felipecrivera.0987@gmail.com>
2024-02-20 09:19:49 -0600

    make pwd and change to select

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import BusinessHours from "./components/business-hours";
 import days from "./days.json";
+import shops from "./shops.json";
 import axios from "axios";
 
 const DemoContainer = styled.div`
@@ -17,12 +18,21 @@ const DemoComponent = styled.div`
 `;
 
 function App() {
-  const [shopName, setShopName] = useState("Codice Farmacia")
-  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [shopName, setShopName] = useState("Codice Farmacia");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [pwd, setPwd] = useState("");
+
+  useEffect(() => {
+    setPwd(prompt("Enter password to access."));
+  }, []);
+
+  useEffect(() => {
+    if (pwd !== "CambioNeoapotek24") setPwd(prompt("Enter password to access."));
+  }, [pwd]);
 
   const updateDays = (dayInd, val) => {
-    days[dayInd] = val
-  }
+    days[dayInd] = val;
+  };
 
   const submitData = () => {
     if (!shopName) {
@@ -32,73 +42,128 @@ function App() {
     let d = [];
     d.push(shopName);
     Object.entries(days.business).forEach(([i, { day, hours }]) => {
-      const ts = []
+      const ts = [];
       hours.forEach((i) => {
         if (i.isOpen) {
-          if ((i['open'] && i['close']))
-            ts.push(i['open'].slice(0, 2) + ":" + i['open'].slice(2) + "-" + i['close'].slice(0, 2) + ":" + i["close"].slice(2));
+          if (i["open"] && i["close"])
+            ts.push(
+              i["open"].slice(0, 2) +
+                ":" +
+                i["open"].slice(2) +
+                "-" +
+                i["close"].slice(0, 2) +
+                ":" +
+                i["close"].slice(2)
+            );
         }
       });
-      d.push(ts.join(', '));
-    })
+      d.push(ts.join(", "));
+    });
     let specialHours = [];
 
     Object.entries(days.special).forEach(([i, { day, hours }]) => {
       hours.forEach((i) => {
         if (i.isOpen) {
-          if ((i['open'] && i['close']))
-            specialHours.push(day.slice(6) + "-" + day.slice(3, 5) + "-" + day.slice(0, 2) + ": " + i['open'].slice(0, 2) + ":" + i['open'].slice(2) + "-" + i['close'].slice(0, 2) + ":" + i["close"].slice(2));
-        }
-        else {
-          specialHours.push(day.slice(6) + "-" + day.slice(3, 5) + "-" + day.slice(0, 2) + ": x")
+          if (i["open"] && i["close"])
+            specialHours.push(
+              day.slice(6) +
+                "-" +
+                day.slice(3, 5) +
+                "-" +
+                day.slice(0, 2) +
+                ": " +
+                i["open"].slice(0, 2) +
+                ":" +
+                i["open"].slice(2) +
+                "-" +
+                i["close"].slice(0, 2) +
+                ":" +
+                i["close"].slice(2)
+            );
+        } else {
+          specialHours.push(
+            day.slice(6) + "-" + day.slice(3, 5) + "-" + day.slice(0, 2) + ": x"
+          );
         }
       });
-    })
-    d.push(specialHours.join(', '));
+    });
+    d.push(specialHours.join(", "));
 
-    axios.post(process.env.REACT_APP_BACKEND_ENDPOINT, d)
+    axios
+      .post(process.env.REACT_APP_BACKEND_ENDPOINT, d)
       .then(function (response) {
         console.log(response);
-        setIsSubmitted(true)
+        setIsSubmitted(true);
       })
       .catch(function (error) {
         console.log(error);
       });
-  }
+  };
+
   return (
-    <div className='App'>
-      <DemoContainer>
-        <h1>Modulo per l'aggiornamento degli orari</h1>
-        {
-          !isSubmitted ?
+    <div className="App">
+      {pwd === "CambioNeoapotek24" ? (
+        <DemoContainer>
+          <h1>Modulo per l'aggiornamento degli orari</h1>
+          {!isSubmitted ? (
             <>
-              <label htmlFor="shop-name">CDC NEO:</label> <input id="shop-name" value={shopName} onChange={(d) => setShopName(d.target.value)} placeholder="Codice Farmacia" />
+              <label htmlFor="shop-name">CDC NEO:</label>
+              <select
+                id="shop-name"
+                value={shopName}
+                onChange={(d) => setShopName(d.target.value)}
+                placeholder="Codice Farmacia"
+                style={{ width: "450px" }}
+              >
+                {shops.map((e) => (
+                  <option value={e}>{e}</option>
+                ))}
+              </select>
               <DemoComponent>
                 <h2>Orari ordinari</h2>
-                <p>Inserisci gli orari ordinari della farmacia, gli orari saranno considerati validi dal momento del caricamento.</p>
-                <BusinessHours timeIncrement={15} updateDays={(v) => updateDays('business', v)} days={days.business} time-increment={15} hourFormat24={true}></BusinessHours>
+                <p>
+                  Inserisci gli orari ordinari della farmacia, gli orari saranno
+                  considerati validi dal momento del caricamento.
+                </p>
+                <BusinessHours
+                  timeIncrement={15}
+                  updateDays={(v) => updateDays("business", v)}
+                  days={days.business}
+                  time-increment={15}
+                  hourFormat24={true}
+                ></BusinessHours>
               </DemoComponent>
               <DemoComponent>
                 <h2>Aperture e chiusure straordinarie</h2>
-                <p>Inserire di seguito tutte le aperture o gli orari straordinari della farmacia. Gli orari fanno riferimento a giorni feriali, turni o cambi di orario straordinario. Selezionare il giorno e indicare gli orari di apertura o la chiusura</p>
+                <p>
+                  Inserire di seguito tutte le aperture o gli orari straordinari
+                  della farmacia. Gli orari fanno riferimento a giorni feriali,
+                  turni o cambi di orario straordinario. Selezionare il giorno e
+                  indicare gli orari di apertura o la chiusura
+                </p>
                 <BusinessHours
                   timeIncrement={15}
-                  updateDays={(v) => updateDays('special', v)}
+                  updateDays={(v) => updateDays("special", v)}
                   datePick={true}
                   days={days.special}
-                  name='holidayHours'
-                  color='#00af0b'
+                  name="holidayHours"
+                  color="#00af0b"
                   hourFormat24={true}
                   time-increment={60}
                 ></BusinessHours>
               </DemoComponent>
               <button onClick={submitData}>Invia i nuovi orari</button>
             </>
-            :
-            <p>Grazie, i nuovi orari sono stati registrati, verranno aggiornati sui nostri sistemi nelle prossime ore.</p>
-        }
-
-      </DemoContainer>
+          ) : (
+            <p>
+              Grazie, i nuovi orari sono stati registrati, verranno aggiornati
+              sui nostri sistemi nelle prossime ore.
+            </p>
+          )}
+        </DemoContainer>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
